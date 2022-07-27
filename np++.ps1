b4055cfb7440ea24dfce256d5f4e23f46328ab7e
tsiswaryalakshmi <99864357+tsiswaryalakshmi@users.noreply.github.com>
2022-07-27 11:20:22 +0530

    Add files via upload

---
+++ np++.ps1
@@ -0,0 +1,5 @@
+﻿Invoke-WebRequest https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.2.1/npp.8.2.1.Installer.exe -OutFile c:\app\npp.8.2.1.Installer.exe
+
+Start-Process ./npp.8.2.1.Installer.exe /S -NoNewWindow -Wait -PassThru
+
+Add-WindowsFeature Web-Server;
